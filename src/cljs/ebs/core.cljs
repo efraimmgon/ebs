e8c869391bd72b6b22db917d3366f4357577ff09
efraimmgon <efraimmaugusto@gmail.com>
2023-07-16 18:26:58 -0400

    move firebase credentials to its own ns

--- src/cljs/ebs/core.cljs
+++ src/cljs/ebs/core.cljs
@@ -7,26 +7,9 @@
    [goog.history.EventType :as HistoryEventType]
    [ebs.ajax :as ajax]
    [ebs.events]
-   [ebs.router :as router]
-   [clojure.string :as string]
-   ["firebase/app" :as firebase]
-   [oops.core :as oops])
+   [ebs.firebase :as init]
+   [ebs.router :as router])
   (:import goog.History))
-
-
-(defn firebase-init []
-  (let [app
-        (oops/ocall firebase "initializeApp"
-                    (clj->js
-                     {:apiKey "AIzaSyAjZLmv-gHHAR_ZMYbJQJCiDrLDDTUEInQ",
-                      :authDomain "manager-bf95c.firebaseapp.com",
-                      :projectId "manager-bf95c",
-                      :storageBucket "manager-bf95c.appspot.com",
-                      :messagingSenderId "1075455170693",
-                      :appId "1:1075455170693:web:b47966b515a03c324331ec",
-                      :measurementId "G-WM3LLG5E4C"}))]
-    (rf/dispatch [:assoc-in [:firebase/app] app])))
-
 
 ;; -------------------------
 ;; Initialize app
@@ -35,7 +18,7 @@
   (rdom/render [#'router/page] (.getElementById js/document "app")))
 
 (defn init! []
-  (firebase-init)
+  (init/firebase-init!)
   (router/start-router!)
   (ajax/load-interceptors!)
   (rf/dispatch-sync [:initialize-app!])
